5b9966b24c39ca8789c65d4895482eb03f187cb8
swillenson <shw58@cornell.edu>
2024-01-29 20:52:00 -0800

    add damping and numba

--- 1D_wave_sim.py
+++ 1D_wave_sim.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
+from numba import jit
 
 L = 1.0 #string length
 t = 1.0 #time total
@@ -12,6 +13,7 @@ v0 = 5.0 #initial velocity
 #guitar string
 plucked_x = 0.2
 plucked_width = 0.1
+damping_factor = 0.01
 
 #make discrete
 nx = 200
@@ -39,15 +41,22 @@ ax.set_ylabel('Amplitude')
 y_lim = 4
 ax.set_ylim(-y_lim,y_lim)
 
-def time_step(frame):
-    global u0, u1
+#create values to be animated, faster using numba
+@jit(nopython = True)
+def time_step(u0, u1, damping_factor, c, dt, dx):
+    damping_term = damping_factor * (u1[1:-1] - 2 * u1[1:-1] + u1[:-2])
     u2 = 2 * (1 - c**2 * dt**2 / dx**2) * u1[1:-1] - u0[1:-1] +\
-        c**2 * dt**2 / dx**2 * (u1[:-2] + u1[2:])
+        c**2 * dt**2 / dx**2 * (u1[:-2] + u1[2:]) - damping_term
     u0 = u1.copy()
     u1[1:-1] = u2
+    return u0, u1
+
+def animate(frame):
+    global u0, u1
+    u0, u1 = time_step(u0, u1, damping_factor, c, dt, dx)
     line.set_ydata(u1)
     return line,
 
 #animate
-animation = FuncAnimation(fig, time_step, frames = range(nt), blit = True)
+animation = FuncAnimation(fig, animate, frames = range(nt), blit = True)
 plt.show()
