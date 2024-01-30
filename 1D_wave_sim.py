7707e84480a06cb5847974859844ee139be826f8
swillenson <shw58@cornell.edu>
2024-01-29 20:09:57 -0800

    guitar pluck

--- 1D_wave_sim.py
+++ 1D_wave_sim.py
@@ -5,21 +5,30 @@ from matplotlib.animation import FuncAnimation
 
 L = 1.0 #string length
 t = 1.0 #time total
-dt = 0.01 #time step
+dt = 0.005 #time step
 c = 1.0 #speed of wave
-v0 = 1.0 #initial velocity
+v0 = 5.0 #initial velocity
+
+#guitar string
+plucked_x = 0.2
+plucked_width = 0.1
 
 #make discrete
-nx = 100
+nx = 200
 nt = int(t / dt)
 
 dx = L / (nx - 1)
 x = np.linspace(0, L, nx)
 
 #initial conditions
+#guitar sim
+u0 = np.exp(-((x - plucked_x) / plucked_width)**2)  # Gaussian pluck
+u1 = u0.copy() + 0.01 * np.sin(np.pi * x / L)  # Adding a sinusoidal initial velocity
 
-u0 = np.sin(2 * np.pi * x) #wave shape
-u1 = u0.copy() + v0 * dt
+#normal sim
+# u0 = np.sin(np.pi * x) #wave shape
+# # u0 = np.linspace(0,0,nx )
+# u1 = u0.copy() + v0 * dt
 
 #plotting
 fig, ax = plt.subplots()
@@ -27,6 +36,8 @@ line, = ax.plot(x, u1)
 ax.set_title('1D Wave Eq')
 ax.set_xlabel('Position')
 ax.set_ylabel('Amplitude')
+y_lim = 4
+ax.set_ylim(-y_lim,y_lim)
 
 def time_step(frame):
     global u0, u1
